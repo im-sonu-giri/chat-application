248a296538a84cf1056c89be97428aff1b7a9c28
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-01-08 21:26:13 +0545

    conversation list and create view

--- chatsystem/cahtapp/views.py
+++ chatsystem/cahtapp/views.py
@@ -18,3 +18,47 @@ class UserListView(generics.ListAPIView):
     serializer_class = UserListSerializer
     permission_classes = [IsAuthenticated]
     
+
+class ConversationListCreateView(generics.ListCreateAPIView):
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return (Conversation.objects.filter(participants=self.request.user).prefetch_related('participants'))
+    
+    def create(self, request, *args, **kwargs):
+        participants_data = request.data.pop('participants', [])
+        
+        if len(participants_data) != 2:
+            return Response(
+                {'error': 'Conversation needs exactly two Participanta'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if str(request.user.id) not in map(str, participants_data):
+            return Response(
+                {
+                    'error': 'you are not participant of this conversation'
+                },
+                status=status.HTTP_403_FORBIDDEN
+            )
+            
+        users = User.objects.filter(id_in=participants_data)
+        if users.count()!=2:
+            return Response(
+                {
+                    'error':"conversation needs two participants"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        existing_conversation = Conversation.objects.filter(
+            participants__id= participants_data[0]
+        ).filter(participants__id=participants_data[1]).distinct()
+        
+        if existing_conversation.exists():
+            return Response(
+                {
+                "error": "conversation already exist"
+                }, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        conversation = Conversation.objects.create()
+        conversation.participants.set(users)
