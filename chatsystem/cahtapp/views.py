e1ac910be742b8743d701806f6371eac42e37c6d
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-01-08 20:58:19 +0545

    create user view

--- chatsystem/cahtapp/views.py
+++ chatsystem/cahtapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .models import *
+from .serializers import *
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
