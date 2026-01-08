fcac84272cfa76eacd169831bc0c275764ef287a
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-01-08 17:01:10 +0545

    custom manager for conversation

--- chatsystem/cahtapp/models.py
+++ chatsystem/cahtapp/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Prefetch #helps to solve the N+1 problem
 
-
+class ConversationManger(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related(
+            Prefetch('participants', queryset=User.objects.only('id', 'username'))
+        )
 
 
 
