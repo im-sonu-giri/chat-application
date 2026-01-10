81c871c7cafac0a1264d3fb9d631ef6d01309f32
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-01-10 12:19:19 +0545

    Jwt authentication

--- chatsystem/cahtapp/customers.py
+++ chatsystem/cahtapp/customers.py
@@ -15,3 +15,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
         query_string = self.scope['query_string'].decode('utf8')
         params = parse_qs(query_string)
         token = params.get('token', [None])[0]
+        
+        if token:
+            try:
+                decode_data = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+                self.user = await self.get_user(decode_data['user_id'])
+                self.scope['user']= self.user
+            except jwt.ExpiredSignatureError:
+                self.close(code=4000)
+                #close the connection if token expired
+                return
+            except jwt.InvalidTokenError:
+                self.close(code=4001)#close if token invalid
+                return
+        else:
+            await self.close(code=4002)#close if token not provided
+            return
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
