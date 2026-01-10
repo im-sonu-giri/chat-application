7b760c9074303d2bd7fd94283f4ab15b7d2d868a
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-01-10 12:01:37 +0545

    token retrieved

--- chatsystem/cahtapp/customers.py
+++ chatsystem/cahtapp/customers.py
@@ -5,3 +5,13 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth  import get_user_model
 from django.conf import settings
 from .models import *
+from urllib.parse import parse_qs
+
+
+User = get_user_model()
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        query_string = self.scope['query_string'].decode('utf8')
+        params = parse_qs(query_string)
+        token = params.get('token', [None])[0]
