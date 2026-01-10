061a4f3af8d8fccd444c0add9764a168ca55aa08
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-01-10 12:33:39 +0545

    disconnect user

--- chatsystem/cahtapp/customers.py
+++ chatsystem/cahtapp/customers.py
@@ -15,10 +15,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
         query_string = self.scope['query_string'].decode('utf8')
         params = parse_qs(query_string)
         token = params.get('token', [None])[0]
-        
+        #validate jwt
         if token:
             try:
                 decode_data = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+                #get authenticated user
                 self.user = await self.get_user(decode_data['user_id'])
                 self.scope['user']= self.user
             except jwt.ExpiredSignatureError:
@@ -31,7 +32,41 @@ class ChatConsumer(AsyncWebsocketConsumer):
         else:
             await self.close(code=4002)#close if token not provided
             return
+        #ADD logic:
+        # Registers this connection into a group
+        # Allows:
+        # Broadcasting messages
+        # Tracking online users
+        # Group notifications
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
+        await self.accept()
+        user_data = await self.get_user_data(self.user)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'online status',
+                'online_user': [user_data],
+                'status': 'online',
+            }
+        )
+    async def disconnect(self, close_code):
+        if hasattr(self, 'room_group_name'):
+            #notify about disconnection
+            user_data = await self.user_data(self.scope['user'])
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                   'type': 'online_status',
+                    'online_user': [user_data],
+                    'status': 'offline',
+                }
+            )
+            await self.channel_layer.group_discard(
+                self.room_group_name,
+                self.channel_layer
+            )
+            
+    
