9f2c7dd5fffaf4d2da5eab85aa7ec6d91a765a14
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2026-01-16 16:27:20 +0545

    home page

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Home =() => {
+    
+    return (
+        <div className="home-container">
+            <header className="home-header"> 
+            <h1> Welcome to sonu's chat application</h1>
+            <p> Connect with Your friends easily</p>
+
+            </header>
+
+        </div>
+    )
+}
+export default Home;
